781047b44b492704a7ae8cc5a0a667320ca48f0b
josecarloshernandezz <60051781+josecarloshernandezz@users.noreply.github.com>
2021-05-13 07:14:34 -0500

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,9 +1,9 @@
 # NYU
-input ("Please enter the number of coins: ")
-quarters = float(input ("# of quarters: "))
-dimes = float(input ("# of dimes: "))
-nickels = float(input ("# of nickels: "))
-pennies = float(input ("# of pennies: "))
+print ("Please enter the number of coins:")
+quarters = float (input ("# of quarters: "))
+dimes = float (input ("# of dimes: "))
+nickels = float (input ("# of nickels: "))
+pennies = float (input ("# of pennies: "))
 dollar = (quarters * 25 + dimes * 10 + nickels * 5 + pennies * 1) /100
 cents =  (quarters * 25 + dimes * 10 + nickels * 5 + pennies * 1) % 100
 print ("The total is", dollar, "dollars and", cents, "cents") 
